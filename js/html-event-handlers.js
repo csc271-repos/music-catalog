ac825f9f658c77f66c88e4ca7f95b5a8ce229b18
csc372 <141356425+csc372@users.noreply.github.com>
2024-11-08 18:50:19 -0500

    Create html-event-handlers.js

---
+++ js/html-event-handlers.js
@@ -0,0 +1,24 @@
+/* 
+  TO DO: 
+  
+      1) Create a function that accepts a Spotify track ID as an argument and contains the following code:
+ 
+         // Get the iframe element that contains the Spotify embedded player
+         var player_iframe = document.getElementById('spotify-player');
+         
+         // Update the iframe source to the clicked track
+         player_iframe.src = `https://open.spotify.com/embed/track/${track_id}`;
+         
+         // Show and animate the player
+         document.getElementById('spotify-player-container').classList.add('visible');          
+      
+      2) Use an HTML event handler attribute on the top record (single) to call this function when clicked. 
+      
+      3) Create a basic function (no parameters or return) that contains the following code:
+
+         // Close the Spotify embedded player by changing its visibility 
+         document.getElementById('spotify-player-container').classList.remove('visible');
+
+      4) Use an HTML event handler attribute on the close button of the Spotify embedded player to call this function when clicked. 
+      
+*/
