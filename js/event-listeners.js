3b7200630fd670b98e477603efa7a5ec0f11f9ab
csc372 <141356425+csc372@users.noreply.github.com>
2024-11-08 19:04:12 -0500

    Update event-listeners.js

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -1,1 +1,15 @@
+// Get all record cards
+var record_cards = document.querySelectorAll('.record-card');
+
+// 
+var close_btn = document.getElementById('close-player');
+
+
+/* 
+  TO DO: 
+  
+      1) Copy and paste BOTH functions you wrote back in the html-event-handlers.js script.
+      
+*/
+
 
