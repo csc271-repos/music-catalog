32ac5eb3a0d2b9605785a630ffbf4564aa8bcab4
csc372 <141356425+csc372@users.noreply.github.com>
2024-11-08 19:06:57 -0500

    Update event-listeners.js

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -1,7 +1,7 @@
 // Get all record cards
 var record_cards = document.querySelectorAll('.record-card');
 
-// 
+// Get the close button of the Spotify embedded player
 var close_btn = document.getElementById('close-player');
 
 
@@ -9,6 +9,9 @@ var close_btn = document.getElementById('close-player');
   TO DO: 
   
       1) Copy and paste BOTH functions you wrote back in the html-event-handlers.js script.
+
+      2) Loop through all records in the third row. For each record, do the following:
+         a) Add an event listener so 
       
 */
 
